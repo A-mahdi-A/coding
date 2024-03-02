c0fe5f435962bc5a4973daca94bcc262f3bcf926
AmirMahdiAsghari <amirmahdiasghari2022@gmail.com>
2024-03-02 19:04:52 +0330

    over sign up page

--- 1/food app for phone/src/App.jsx
+++ 1/food app for phone/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import StartPage from './pasges/StarrPage';
 import Login from './pasges/login';
+import PageSignUp from './pasges/Signup';
 
 import './App.scss'
 
@@ -9,11 +10,14 @@ function App() {
 
   return (
     <>
+
       <Routes>
         {/* <Route path='' element='' /> */}
 
         <Route path='/' element={<StartPage />} />
-        <Route path='/login' element={<Login />} /> 
+        <Route path='/login' element={<Login />} />
+        <Route path='/Sign-Up' element={<PageSignUp />
+        } />
 
 
 
--- 1/food app for phone/src/pasges/Signup.jsx
+++ 1/food app for phone/src/pasges/Signup.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from '../assets/image/Group 3.png'
-import '../styles/loginPage.scss';
-import { Link } from "react-router-dom";
-function pageLogin() {
+import { Link } from "react-router-dom"; import Logo from '../assets/image/Group 3.png'
+import '../styles/Sign-up.scss';
+function PageSignUp() {
     return (
-        <div id="parent">
+        <>
+
             <section id="top">
                 <div id="p-imge"><img src={Logo} alt="logo web site" /></div>
                 <div id="p-login-sinUP">
@@ -22,13 +22,15 @@ function pageLogin() {
                     <h6>Password</h6>
                     <input type="Password" id="Passwords" required />
                 </div>
+                <div className="Confirm password" id="passeord">
+                    <h6> Confirm password</h6>
+                    <input type="Password" id="Passwords" required />
+                </div>
             </section>
             {/* ----------------------------- */}
-            <section id="Forgot"> <div><Link >Forgot passcode?</Link></div>     </section>
-            {/* ---=-=--=-=-==----==---==-=-=-=-=- */}
-            <section id="buttonL"><button><Link>Login</Link></button></section>
-          
-        </div>
-    );
+
+            <section id="buttonL"><button><Link>Sign Up</Link></button></section>
+        </>
+    )
 }
-export default pageLogin;
+export default PageSignUp

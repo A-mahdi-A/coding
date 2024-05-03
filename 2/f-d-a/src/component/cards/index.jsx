7c007f0deeb64fbbea5e8285507a4da707188f18
AmirMahdiAsghari <amirmahdiasghari2022@gmail.com>
2024-05-03 17:28:24 +0330

    /

--- 2/f-d-a/src/component/cards/index.jsx
+++ 2/f-d-a/src/component/cards/index.jsx
@@ -2,18 +2,21 @@
 import React from "react";
 import "./style.scss";
 import FoodImage from "../../assets/image/food.svg";
+import { Link } from "react-router-dom";
 function Card() {
   return (
     <>
       <section id="cardsParent">
-        <div id="im">
-          <img src={FoodImage} alt="" />
-        </div>
-        <div>
-          <h2 id="foodName">Veggie <br />tomato mix</h2>
-          <p 
-          id="Price">N1,900</p>
-        </div>
+        <Link>
+          <div id="im">
+            <img src={FoodImage} alt="" />
+          </div>
+          <div>
+            <h2 id="foodName">Veggie <br />tomato mix</h2>
+            <p
+              id="Price">N1,900</p>
+          </div>
+        </Link>
       </section>
     </>
   );

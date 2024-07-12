339b20b560a3118e4623e24ccd0d18b9e3ce9462
AmirMahdiAsghari <amirmahdiasghari2022@gmail.com>
2024-07-12 04:26:44 +0330

    const Variable

--- js/Variable/script.js
+++ js/Variable/script.js
@@ -1,5 +1,10 @@
-// Variable var
+// Variable var we can chenge Variable input
 var x = 12;
 var z = 1;
 var c = x + z;
-console.log(x,z,c)
+console.log(x, z, c)
+// const Variable we cant cheng input
+const A = 11;
+const B = 13;
+const q = A + B;
+console.log(q)
